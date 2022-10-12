3b0da250c14c02b9c0dea0f3dc55df1fc93403b4
Mr-big-cake <63250784+Mr-big-cake@users.noreply.github.com>
2022-10-12 19:24:58 +0300

    Add files via upload

---
+++ kernel_2_6.cu
@@ -0,0 +1,95 @@
+﻿#ifndef __CUDACC__ 
+#define __CUDACC__
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <curand_kernel.h>
+#include <curand.h>
+#include <device_functions.h>
+
+
+#define N 30
+#define SIZE 256
+#define MIN 0
+#define MAX 100
+#define DIF (MAX - MIN)
+
+__global__ void kernelFirst(curandState* state, unsigned long seed, int n)
+{
+    int id = threadIdx.x + blockDim.x * blockIdx.x;
+    if (id < n)
+        curand_init(seed, id, 0, &state[id]);
+}
+
+__global__ void kernelGenerate(curandState* gState, float* result, int count)
+{
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    if (idx < count) {
+        curandState localState = gState[idx];
+        float RANDOM = curand_uniform(&localState);
+        gState[idx] = localState;
+        result[idx] = RANDOM;
+    }
+}
+
+void cpuGenetate(float* result, int count)
+{
+    for (int i = 0; i < count; i++) 
+    {
+        result[i] = MIN + rand() % MAX + 1;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    srand(time(NULL));
+    float* dev_result;
+    float* result = (float*)malloc(DIF * sizeof(float));
+    float* resultCPU = (float*)malloc(DIF * sizeof(float));
+    cudaEvent_t start, stop;
+    float gpuTime = 0.0f;
+    curandState* devStates;
+
+
+    if (cudaMalloc(&devStates, DIF * sizeof(curandState)))
+        printf("Error: cudaMalloc1\n");
+    if (cudaMalloc(&dev_result, DIF * sizeof(float)))
+        printf("Error: cudaMalloc2\n");
+
+    kernelFirst << < (DIF + SIZE - 1) / SIZE, SIZE >> > (devStates, time(NULL), DIF);
+    if (cudaGetLastError() != cudaSuccess) printf("Error: Kernel");
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
+
+    kernelGenerate << < (DIF + SIZE - 1) / SIZE, SIZE >> > (devStates, dev_result, DIF);
+    if (cudaGetLastError() != cudaSuccess) printf("Error: Kernel");
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&gpuTime, start, stop);
+    printf("Time GPU = %.2f millseconds\n", gpuTime);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    int start2, time2;
+    start2 = clock();
+
+    cpuGenetate(resultCPU, DIF);
+
+    time2 = clock() - start2;
+    printf("Time CPU = %d millseconds\n", time2);
+
+    if (cudaMemcpy(result, dev_result, sizeof(float) *DIF, cudaMemcpyDeviceToHost) != cudaSuccess)
+        printf("Error: cudaMemcpy3\n");
+    printf("Random numbers: \n");
+    for (int i = 0; i < N; i++)
+        printf("@-> %f\n", result[i]*DIF + MIN);
+
+
+
+    return 0;
+}
---
+++ kernel_2_my11.cu
@@ -0,0 +1,93 @@
+﻿#ifndef __CUDACC__ 
+#define __CUDACC__
+#endif
+#include <device_functions.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <math.h>
+#include <stdio.h>
+#include <ctime> 
+
+
+#define countThreads 1024
+#define countBlocks 9
+#define N 900;
+
+__constant__ long long dev_constN;
+__global__ void _Perfect_GPU(long long * result, long long maxValue)
+{
+	long long idx = blockDim.x * blockIdx.x + threadIdx.x + 2;
+	if (idx > maxValue) return;
+	long long sum = 0;
+	*result = 0;
+	for (long long j = 1; j < idx; j++)
+		if (idx % j == 0)
+			sum += j;
+	if (sum == idx) {
+		printf("GPU-->%d\n", idx);
+		result[1 + *result] = idx;
+	}
+}
+
+void _Perfect_CPU(long long n)
+{
+	long long i, j, s;
+	for (i = 2; i < n; i++)
+	{
+		s = 0;
+		for (j = 1; j < i; j++)
+			if (i % j == 0)
+				s += j;
+		if (s == i)
+			printf("CPU-->%lld\n", i);
+	}
+}
+
+void main()
+{
+	long long* result = (long long*) malloc(sizeof(long long) * 5 );
+	long long* dev_result;
+
+	long long constN = N;
+
+
+	if (cudaMemcpyToSymbol(dev_constN, &constN, sizeof(constN), 0, cudaMemcpyHostToDevice) != cudaSuccess)
+		printf("Error: cudaMemcpyToSymbol\n");
+	
+	if (cudaMalloc(&dev_result, sizeof(long long) * 5) != cudaSuccess)
+		printf("Error: cudaMalloc\n");
+
+
+	if (cudaGetLastError() != cudaSuccess)
+		printf("Error: Kernel\n");
+
+	cudaEvent_t start, stop; 
+	float gpuTime = 0.0f;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop); 
+	cudaEventRecord(start, 0); 
+
+	_Perfect_GPU <<<countBlocks, countThreads >> > (dev_result, constN);
+
+	if (cudaGetLastError() != cudaSuccess)
+		printf("Error: Kernal");
+
+	cudaEventRecord(stop, 0); 
+	cudaEventSynchronize(stop); 
+	cudaEventElapsedTime(&gpuTime, start, stop); 
+	printf("Time GPU = %.2f millseconds\n", gpuTime);
+	cudaEventDestroy(start); 
+	cudaEventDestroy(stop);
+
+	if (cudaMemcpy(result, dev_result, sizeof(long long) * 5, cudaMemcpyDeviceToHost) != cudaSuccess)
+		printf("Error: cudaMemcpy2\n");
+
+	int start2, time2;
+	start2 = clock();
+
+	_Perfect_CPU(9000);
+
+	time2 = clock() - start2;
+	printf("Time CPU = %d millseconds\n", time2);
+}
